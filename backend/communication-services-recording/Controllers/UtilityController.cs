243c0414478afef0400f5d9b9b837d415f3845da
Kiran Reddy <v-kiranreddy@microsoft.com>
2023-10-09 22:53:57 +0530

    implemented api to get automation sdk version

---
+++ backend/communication-services-recording/Controllers/UtilityController.cs
@@ -0,0 +1,40 @@
+﻿using System.Xml.Linq;
+using System.Xml;
+
+namespace communication_services_recording.Controllers
+{
+    [Route("api/utility")]
+    [ApiController]
+    public class UtilityController : ControllerBase
+    {
+        [HttpGet]
+        [Route("get/sdk/version")]
+        public async Task<ActionResult> GetPackageLatestVersion()
+        {
+            string projectFilePath = $"{Directory.GetCurrentDirectory()}\\communication-services-recording.csproj";
+            string packageName = "Azure.Communication.CallAutomation";
+            var result = new Dictionary<string, string>();
+            try
+            {
+                XDocument doc = XDocument.Load(projectFilePath);
+
+                var packageReference = doc.Descendants("PackageReference")
+                    .FirstOrDefault(pr => pr.Attribute("Include")?.Value == packageName);
+
+                if (packageReference != null)
+                {
+                    string packageVersion = packageReference.Attribute("Version")?.Value;
+                   
+                    result.Add("packageName", packageName);
+                    result.Add("packageVersion", packageVersion);
+                }
+            }
+            catch (XmlException e)
+            {
+                Console.WriteLine($"Error parsing {projectFilePath}: {e.Message}");
+            }
+
+            return Ok(result);
+        }
+    }
+}
